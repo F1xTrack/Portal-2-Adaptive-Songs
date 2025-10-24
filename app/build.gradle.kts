4a2a96a50fb4ba15d65c10fd9decca1eda53e8d5
F1xTrack <vladik130310@gmail.com>
2025-10-24 22:06:53 +0300

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 30
         targetSdk = 33
         versionCode = 3
-        versionName = "1.3.1"
+        versionName = "1.3.2"
     }
     buildFeatures {
         viewBinding = true
